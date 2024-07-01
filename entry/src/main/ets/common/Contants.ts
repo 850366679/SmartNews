fc15b1a5b71bcc8c246eea85e7b7333ab348534e
rayms <850366679@qq.com>
2024-07-02 01:11:29 +0800

    first commit

---
+++ entry/src/main/ets/common/Contants.ts
@@ -0,0 +1,5 @@
+// 通用TAG
+export const TAG = 'smart_news'
+
+// 通用Preferences名字
+export const COMMON_PREF_NAME = "common_preferences_name"
---
+++ entry/src/main/ets/data/enum/NewsCategory.ts
@@ -0,0 +1,10 @@
+/**
+ * 请求主页数据类型
+ */
+export enum NewsCategory {
+  RECOMMEND, // 推荐
+  HOT, // 热榜
+  SCIENCE, // 科技
+  SPORTS, // 体育
+  FOOD, // 美食
+}
